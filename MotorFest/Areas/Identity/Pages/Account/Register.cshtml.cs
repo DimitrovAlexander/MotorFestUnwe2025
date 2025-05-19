d3f048274bfcd959625545814ac31494a50b7a42
DimitrovAlexander <alexander_dimitrov@yahoo.com>
2025-05-19 10:27:28 +0300

    Added Admin rights

--- MotorFest/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MotorFest/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -139,7 +139,7 @@ namespace MotorFest.Areas.Identity.Pages.Account
                 };
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
-                var roleAssignmentResult = await _userManager.AddToRoleAsync(user, Input.Role);
+                var roleAssignmentResult = await _userManager.AddToRoleAsync(user, "Administrator");
                 if (result.Succeeded)
                 {
 
